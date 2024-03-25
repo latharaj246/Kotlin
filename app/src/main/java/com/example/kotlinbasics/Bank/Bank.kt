0ae05c73157c5cfb5c5e8fbd92785977cc33c9ac
Latha <latha.r@mpokket.com>
2024-03-25 22:33:46 +0530

    bank

--- app/src/main/java/com/example/kotlinbasics/Bank/Bank.kt
+++ app/src/main/java/com/example/kotlinbasics/Bank/Bank.kt
@@ -1,4 +1,15 @@
 package com.example.kotlinbasics.Bank
 
-class Bank {
+fun main() {
+
+    val lathaBankaccount = BankAccount("Latha" , 5000.00)
+    lathaBankaccount.deposit(55.555)
+    lathaBankaccount.displayTransactionHistory()
+    lathaBankaccount.withdraw(5.5)
+    lathaBankaccount.displayTransactionHistory()
+    lathaBankaccount.deposit(5.5)
+    println("${lathaBankaccount.balance}")
+    println(lathaBankaccount.accoutHolder)
+
+
 }
---
+++ app/src/main/java/com/example/kotlinbasics/Bank/BankAccount.kt
@@ -0,0 +1,32 @@
+package com.example.kotlinbasics.Bank
+
+class BankAccount (var accoutHolder : String, var balance : Double) {
+
+
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double) {
+        balance += amount
+        //balance = balance + amount
+        transactionHistory.add("$accoutHolder deposited $amount ")
+    }
+
+    fun withdraw(amount: Double) {
+        if (amount <= balance) {
+            //we can withdraw
+            balance  -= amount
+            transactionHistory.add("$accoutHolder withdrew $amount")
+        } else {
+            //we can not withdraw
+            println("you can not withdraw")
+        }
+
+    }
+
+    fun displayTransactionHistory() {
+        println("transaction history for $accoutHolder")
+        for(item in transactionHistory){
+            println(transactionHistory)
+        }
+    }
+}
